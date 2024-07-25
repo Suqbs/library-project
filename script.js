ed99723e645ec748222c169fd3584cca0abcd924
Furkan Karadeniz <95278326+Suqbs@users.noreply.github.com>
2024-07-25 12:38:44 +0300

    Add basic properties to the constructor

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const myLibrary = [];
 
-function Book() {
+function Book(title, author, pages, read) {
   // the constructor...
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
 function addBookToLibrary() {
   // do stuff here
+
 }
