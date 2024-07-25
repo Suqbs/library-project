bda4ba45e5a51951591503023010b943be407c7e
Furkan Karadeniz <95278326+Suqbs@users.noreply.github.com>
2024-07-25 17:46:56 +0300

    Add function for displaying books

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
-Book.prototype.newBook = function() {
-    addBookToLibrary(this);
-}
+Book.prototype.newBook = function () {
+  addBookToLibrary(this);
+};
 
 function addBookToLibrary(Book) {
   // do stuff here
@@ -24,5 +24,35 @@ const theHobbit = new Book(
   "not read yet"
 );
 
+function displayBooks() {
+
+  const bookContainer = document.getElementById("book-container");
+
+  for (const bookObj of myLibrary) {
+    const book = document.createElement("div");
+    const title = document.createElement("h1");
+    const author = document.createElement("p");
+    const pages = document.createElement("p");
+    const read = document.createElement("p");
+
+    book.classList.add("card");
+    bookContainer.appendChild(book);
+
+    title.textContent = bookObj.title;
+    author.textContent = `Author: ${bookObj.author}`;
+    pages.textContent = `Pages: ${bookObj.pages}`;
+    read.textContent = `Read: ${bookObj.read}`;
+
+    book.appendChild(title);
+    book.appendChild(author);
+    book.appendChild(pages);
+    book.appendChild(read);
+  }
+
+}
+
 theHobbit.newBook();
+
 console.log(myLibrary);
+
+displayBooks();
