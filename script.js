8027c17f0e2fb2aae545867664ee44556afc133d
Furkan Karadeniz <95278326+Suqbs@users.noreply.github.com>
2024-07-26 19:46:47 +0300

    Add checkbox to change read status

--- script.js
+++ script.js
@@ -12,6 +12,10 @@ Book.prototype.newBook = function () {
   addBookToLibrary(this);
 };
 
+Book.prototype.toggleRead = function (checkbox) {
+  return checkbox.checked ? "Read" : "Not Read Yet";
+};
+
 function addBookToLibrary(Book) {
   // do stuff here
   myLibrary.push(Book);
@@ -24,29 +28,47 @@ function displayBooks(bookObj) {
   const title = document.createElement("h1");
   const author = document.createElement("p");
   const pages = document.createElement("p");
-  const read = document.createElement("p");
+  const readContainer = document.createElement("div");
+  const readLabel = document.createElement("label");
+  let readCheckbox = document.createElement("input");
   const removeButton = document.createElement("button");
-//   removeButton.setAttribute("id", "remove-book-button");
 
+  readLabel.setAttribute("for", "read");
+  readCheckbox.setAttribute("id", "read");
+  readCheckbox.setAttribute("type", "checkbox");
+  readCheckbox.setAttribute("name", "read-toggle");
+
+  readContainer.classList.add("flex-label");
+  readCheckbox.classList.add("read-checkbox");
   book.classList.add("card");
-  bookContainer.appendChild(book);
-//  book.dataset.indexValue = (myLibrary.length - 1);
 
   title.textContent = bookObj.title;
   author.textContent = `Author: ${bookObj.author}`;
   pages.textContent = `Pages: ${bookObj.pages}`;
-  read.textContent = `Read: ${bookObj.read}`;
+  readLabel.textContent = `${bookObj.read}`;
   removeButton.textContent = "REMOVE BOOK";
+
+  if (readLabel.textContent === "Read") readCheckbox.checked ^= 1;
+
+  readContainer.appendChild(readLabel);
+  readContainer.appendChild(readCheckbox);
 
   book.appendChild(title);
   book.appendChild(author);
   book.appendChild(pages);
-  book.appendChild(read);
+  book.appendChild(readContainer);
   book.appendChild(removeButton);
+
+  bookContainer.appendChild(book);
 
   removeButton.addEventListener("click", () => {
     book.remove();
-  })
+  });
+
+  readCheckbox.addEventListener("change", (e) => {
+    bookObj.read = bookObj.toggleRead(e.target);
+    readLabel.textContent = bookObj.read;
+  });
 }
 
 const newBookButton = document.getElementById("new-book-button");
@@ -66,7 +88,6 @@ modalSubmitButton.addEventListener("click", (e) => {
   const authorInput = document.querySelector("#author");
   const pagesInput = document.querySelector("#pages");
   const readInput = document.querySelector("#read-select");
-//   const regex = /^[0-9]*$/;
 
   const newBookObj = new Book(
     titleInput.value,
@@ -82,7 +103,7 @@ modalSubmitButton.addEventListener("click", (e) => {
     }
   }
 
-  if ((pagesInput.value === "")) {
+  if (pagesInput.value === "") {
     alert("Please enter a valid page number");
     return false;
   }
